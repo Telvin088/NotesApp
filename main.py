3caba04076041cfddf40be85513e888560713ebe
TelvinMaina <83200575+Telvin088@users.noreply.github.com>
2025-07-06 16:46:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -105,4 +105,4 @@ def notes():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
